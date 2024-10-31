48d7bcd26770fffe3520c365b7ad240b7c874d07
Tiffany Choe <tchoe@aquaphoenixsci.com>
2024-10-30 20:53:02 -0400

    example

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const PORT = 3000;
 
 app.use(express.json());
 
+// example of GET API
+app.get('/api/movies', (req, res) => {
+    // write sql query to get all movies and return in response
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
